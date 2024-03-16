9a3736b7a78cbd399aeebb06ffe7731762cb6aac
ilman <ilmanfrasetya@gmail.com>
2024-03-16 09:55:35 +0700

    good one

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Put } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +8,17 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+  @Post()
+  getPost() {
+    return {
+      date: 'post',
+    };
+  }
+  @Put()
+  getUpdate() {
+    return {
+      data: 'hello put',
+    };
   }
 }
